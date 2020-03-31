ab092cfeea3b478db546c846dcd371797168285c
fahadmak <f.faraqhan@gmail.com>
2020-03-31 11:39:40 -0500

    Create a user and get a user

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,32 @@
+package com.example.demo.services;
+
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAllUser() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public User getUserById(Long id)  {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+}
